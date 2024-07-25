e36728af338f74645e8b5448a20be0f24097d970
Jio (Geo) <reppjio@gmail.com>
2024-07-25 00:12:29 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const menuIcon = document.querySelector('#menu-icon');
-  const navbar = document.querySelector('.my-navbar');
-  const sections = document.querySelectorAll('section');
-  const navLinks = document.querySelectorAll('header nav a');
+  const menuIcon = document.querySelector("#menu-icon");
+  const navbar = document.querySelector(".my-navbar");
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("header nav a");
 
   const track = document.querySelector(".slide-track");
   const items = Array.from(document.querySelectorAll(".project-item"));
@@ -10,45 +10,51 @@ document.addEventListener("DOMContentLoaded", () => {
   const overlay = document.querySelector(".overlay");
   const modal = document.querySelector(".connect-modal");
   const closeModalBtn = document.querySelector(".close-modal");
-  const openModalBtn = document.querySelector(".connect-btn");
+  const openModalBtns = document.querySelectorAll(".connect-btn");
+
+  openModalBtns.forEach((button) => {
+    button.addEventListener("click", openModal);
+  });
+
+  function openModal(event) {
+    overlay.classList.remove("hidden");
+    modal.classList.remove("hidden");
+    event.preventDefault();
+  }
+
+  function closeModal() {
+    overlay.classList.add("hidden");
+    modal.classList.add("hidden");
+  }
+
+  closeModalBtn.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
 
   window.onscroll = () => {
-    sections.forEach(sec => {
+    sections.forEach((sec) => {
       let top = window.scrollY;
       let offset = sec.offsetTop - 150;
       let height = sec.offsetHeight;
-      let id = sec.getAttribute('id');
+      let id = sec.getAttribute("id");
 
       if (top >= offset && top < offset + height) {
-        navLinks.forEach(links => {
-          links.classList.remove('active');
-          document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        navLinks.forEach((links) => {
+          links.classList.remove("active");
+          document
+            .querySelector("header nav a[href*=" + id + "]")
+            .classList.add("active");
         });
       }
     });
   };
 
   menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
   };
-
-  const openModal = () => {
-    overlay.classList.remove("hidden");
-    modal.classList.remove("hidden");
-  };
-
-  const closeModal = () => {
-    overlay.classList.add("hidden");
-    modal.classList.add("hidden");
-  };
-
-  closeModalBtn.addEventListener("click", closeModal);
-  overlay.addEventListener("click", closeModal);
-  openModalBtn.addEventListener("click", openModal);
 
   const cloneItems = (items) => {
-    items.forEach(item => {
+    items.forEach((item) => {
       const clone = item.cloneNode(true);
       clone.style.cssText = window.getComputedStyle(item).cssText;
       track.appendChild(clone);
@@ -58,22 +64,21 @@ document.addEventListener("DOMContentLoaded", () => {
   cloneItems(items);
 
   const startAnimation = () => {
-    track.style.animationPlayState = 'running';
+    track.style.animationPlayState = "running";
   };
 
-  track.style.animationPlayState = 'paused';
+  track.style.animationPlayState = "paused";
 
   setTimeout(startAnimation, 1500);
 
   track.addEventListener("mouseover", () => {
-    track.style.animationPlayState = 'paused';
+    track.style.animationPlayState = "paused";
   });
 
   track.addEventListener("mouseout", () => {
-    track.style.animationPlayState = 'running';
+    track.style.animationPlayState = "running";
   });
 
-  // Swipe Functionality
   let startX = 0;
   let scrollLeft = 0;
   let isDown = false;
@@ -82,28 +87,27 @@ document.addEventListener("DOMContentLoaded", () => {
     isDown = true;
     startX = e.pageX - track.offsetLeft;
     scrollLeft = track.scrollLeft;
-    track.style.cursor = 'grabbing';
+    track.style.cursor = "grabbing";
   });
 
   track.addEventListener("mouseleave", () => {
     isDown = false;
-    track.style.cursor = 'grab';
+    track.style.cursor = "grab";
   });
 
   track.addEventListener("mouseup", () => {
     isDown = false;
-    track.style.cursor = 'grab';
+    track.style.cursor = "grab";
   });
 
   track.addEventListener("mousemove", (e) => {
     if (!isDown) return;
     e.preventDefault();
     const x = e.pageX - track.offsetLeft;
-    const walk = (x - startX) * 2; // Scroll-fast
+    const walk = (x - startX) * 2;
     track.scrollLeft = scrollLeft - walk;
   });
 
-  // Touch events for mobile devices
   let startTouchX = 0;
   let endTouchX = 0;
 
